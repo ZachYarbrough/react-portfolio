9695c09846c2c18b8c05423814155955b93821cc
Zach Yarbrough <zachyarbro@gmail.com>
2021-11-08 22:45:23 -0600

    cleaned resume code

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = (props) => {
             {/* Bio */}
             <div>
                 <h2 className='font-size-24 font-weight-bold border-bottom'>Bio</h2>
-                <p>A Full Stack Developer who specializes in JavaScript Technologies across the MERN stack. I have experience building complete web applications with backend APIs and responsive designs.</p>
+                <p>I am a Full Stack Developer who specializes in JavaScript Technologies across the MERN stack. I have experience building complete web applications with backend APIs and responsive designs.</p>
                 <p className='d-flex align-items-start'><span className='alert alert-primary mr-5 py-0 px-5'>1999</span> <span>Born in Houston, Texas.</span></p>
                 <p className='d-flex align-items-start'><span className='alert alert-primary mr-5 py-0 px-5'>2021</span> <span>Graduated from The University of Texas at Austin with a Bachelors in Arts and Entertainment Technologies. </span></p>
                 <p className='d-flex align-items-start'><span className='alert alert-primary mr-5 py-0 px-5'>2021</span> <span>Completed Full Stack Web Developement Bootcamp from The Unviersity of Texas at Austin.</span></p>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,6 +10,10 @@ const Resume = (props) => {
         setProjectSelected
     } = props
 
+    const languages = ['HTML', 'CSS', 'JavaScript', 'C#', 'Swift'];
+    const technicalSkills = ['React', 'Git', 'NodeJS', 'ExpressJS', 'MongoDB', 'Jest', 'Bootstrap', 'AJAX', 'MySQL', 'Handlebars', 'Halfmoon'];
+    const nonTechnicalSkills = ['Agile Scrum Methodology', 'Object Oriented Design', 'Model View Controller', 'Flexible Learner'];
+
     return (
         <FadeIn className='content'>
             {/* Summary */}
@@ -26,35 +30,25 @@ const Resume = (props) => {
                 <p className='d-flex align-items-start'>
                     <span className='alert alert-primary mt-5 mr-5 py-0 px-5'>Languages</span>
                     <span className='d-flex flex-wrap'>
-                        <span className="alert mt-5 mr-5 py-0 px-5">HTML</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">CSS</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">JavaScript</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">C#</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Swift</span>
+                        {languages.map((language) => (
+                            <span key={language} className="alert mt-5 mr-5 py-0 px-5">{language}</span>
+                        ))}
                     </span>
                 </p>
                 <p className='d-flex align-items-start'>
                     <span className='alert alert-primary mt-5 mr-5 py-0 px-5'>Technical</span>
                     <span className='d-flex flex-wrap'>
-                        <span className="alert mt-5 mr-5 py-0 px-5">React</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Git</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">NodeJS</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">ExpressJS</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">MongoDB</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Jest</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Bootstrap</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">AJAX</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">MySQL</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Handlebars</span>
+                        {technicalSkills.map((skill) => (
+                            <span key={skill} className="alert mt-5 mr-5 py-0 px-5">{skill}</span>
+                        ))}
                     </span>
                 </p>
                 <p className='d-flex align-items-start'>
                     <span className='nowrap-div alert alert-primary mt-5 mr-5 py-0 px-5'>Non Technical</span>
                     <span className='d-flex flex-wrap'>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Agile Scrum Methodology</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Object Oriented Design</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Flexible Learner</span>
-                        <span className="alert mt-5 mr-5 py-0 px-5">Model View Controller</span>
+                        {nonTechnicalSkills.map((skill) => (
+                            <span key={skill} className="alert mt-5 mr-5 py-0 px-5">{skill}</span>
+                        ))}
                     </span>
                 </p>
             </div>
@@ -69,115 +63,114 @@ const Resume = (props) => {
                         setBreadcrumbState('Resume');
                         setProjectSelected(currentProjects[0]);
                     }} className=' ml-10 btn font-weight-bold'>
-                    <span className='mr-5'>View Project</span>
-                    <i className='fa fa-angle-right font-size-12' aria-hidden='true' />
-                </a>
-                <p className='d-flex align-items-start'>
-                    <span className='alert alert-primary mr-10 py-0 px-5'>Website</span>
-                    <a href='https://reploy.herokuapp.com/' target='_'>
-                        <span className='mr-5'>https://reploy.herokuapp.com</span>
-                        <i className='fa fa-external-link-alt' aria-hidden="true"></i>
+                        <span className='mr-5'>View Project</span>
+                        <i className='fa fa-angle-right font-size-12' aria-hidden='true' />
                     </a>
-                </p>
-                <p className='d-flex align-items-start'>
-                    <span className='alert alert-primary mr-10 py-0 px-5'>Source</span>
-                    <a href='https://github.com/abjj1999/Job-Tracker' target='_'>
-                        <span className='mr-5'>https://github.com/abjj1999/Job-Tracker</span>
-                        <i className='fa fa-external-link-alt' aria-hidden="true"></i>
-                    </a>
-                </p>
-                <p className='d-flex align-items-start'>
-                    <span className='alert alert-primary mr-5 py-0 px-5'>Stack</span>
-                    <span className='d-flex flex-wrap'>
-                        <span className="alert mr-5 py-0 px-5">Handlebars</span>
-                        <span className="alert mr-5 py-0 px-5">MySQL</span>
-                        <span className="alert mr-5 py-0 px-5">ExpressJS</span>
-                        <span className="alert mr-5 py-0 px-5">NodeJS</span>
-                    </span>
-                </p>
-            </div>
-            <div>
-                <span className='font-size-18 font-weight-bold'>Lyricado - Music Lyric Quiz</span>
-                <a onClick={() => {
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-10 py-0 px-5'>Website</span>
+                        <a href='https://reploy.herokuapp.com/' target='_'>
+                            <span className='mr-5'>https://reploy.herokuapp.com</span>
+                            <i className='fa fa-external-link-alt' aria-hidden="true"></i>
+                        </a>
+                    </p>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-10 py-0 px-5'>Source</span>
+                        <a href='https://github.com/abjj1999/Job-Tracker' target='_'>
+                            <span className='mr-5'>https://github.com/abjj1999/Job-Tracker</span>
+                            <i className='fa fa-external-link-alt' aria-hidden="true"></i>
+                        </a>
+                    </p>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>Stack</span>
+                        <span className='d-flex flex-wrap'>
+                            {currentProjects[0].stack.map((stack) => (
+                                <span key={stack} className="alert mr-5 py-0 px-5">{stack}</span>
+                            ))}
+                        </span>
+                    </p>
+                </div>
+                <div>
+                    <span className='font-size-18 font-weight-bold'>Lyricado - Music Lyric Quiz</span>
+                    <a onClick={() => {
                         setResumeSelected(false);
                         setBreadcrumbState('Resume');
                         setProjectSelected(currentProjects[1]);
                     }} className=' ml-10 btn font-weight-bold'>
-                    <span className='mr-5'>View Project</span>
-                    <i className='fa fa-angle-right font-size-12' aria-hidden='true' />
-                </a>
-                <p className='d-flex align-items-start'>
-                    <span className='alert alert-primary mr-10 py-0 px-5'>Website</span>
-                    <a href=' https://zachyarbrough.github.io/music-lyric-quiz/' target='_'>
-                        <span className='mr-5'> https://zachyarbrough.github.io/music-lyric-quiz</span>
-                        <i className='fa fa-external-link-alt' aria-hidden="true"></i>
+                        <span className='mr-5'>View Project</span>
+                        <i className='fa fa-angle-right font-size-12' aria-hidden='true' />
                     </a>
-                </p>
-                <p className='d-flex align-items-start'>
-                    <span className='alert alert-primary mr-10 py-0 px-5'>Source</span>
-                    <a href='https://github.com/ZachYarbrough/music-lyric-quiz' target='_'>
-                        <span className='mr-5'>https://github.com/ZachYarbrough/music-lyric-quiz</span>
-                        <i className='fa fa-external-link-alt' aria-hidden="true"></i>
-                    </a>
-                </p>
-                <p className='d-flex align-items-start'>
-                    <span className='alert alert-primary mr-5 py-0 px-5'>Stack</span>
-                    <span className='d-flex flex-wrap'>
-                        <span className="alert mr-5 py-0 px-5">HTML</span>
-                        <span className="alert mr-5 py-0 px-5">CSS</span>
-                        <span className="alert mr-5 py-0 px-5">JavaScript</span>
-                    </span>
-                </p>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-10 py-0 px-5'>Website</span>
+                        <a href=' https://zachyarbrough.github.io/music-lyric-quiz/' target='_'>
+                            <span className='mr-5'> https://zachyarbrough.github.io/music-lyric-quiz</span>
+                            <i className='fa fa-external-link-alt' aria-hidden="true"></i>
+                        </a>
+                    </p>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-10 py-0 px-5'>Source</span>
+                        <a href='https://github.com/ZachYarbrough/music-lyric-quiz' target='_'>
+                            <span className='mr-5'>https://github.com/ZachYarbrough/music-lyric-quiz</span>
+                            <i className='fa fa-external-link-alt' aria-hidden="true"></i>
+                        </a>
+                    </p>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>Stack</span>
+                        <span className='d-flex flex-wrap'>
+                            {currentProjects[1].stack.map((stack) => (
+                                <span key={stack} className="alert mr-5 py-0 px-5">{stack}</span>
+                            ))}
+                        </span>
+                    </p>
+                </div>
             </div>
-        </div>
 
-            {/* Experience */ }
-    <div className='d-flex flex-column'>
-        <h2 className='font-size-24 font-weight-bold border-bottom'>Experience</h2>
-        <span className='font-size-18 font-weight-bold'>StoryFit</span>
-        <div className='d-flex flex-column align-items-start'>
-            <p className='d-flex align-items-start'>
-                <span className='alert alert-primary mr-5 py-0 px-5'>Intern</span>
-                <span className='alert alert-primary mr-5 py-0 px-5'>Jan 2020 - May 2020</span>
-            </p>
-            <span>Utilized critical thinking to regulate movie scripts and format them correctly to ensure AI runs smoothly when determining optimal markets and audience</span>
-        </div>
-    </div>
+            {/* Experience */}
+            <div className='d-flex flex-column'>
+                <h2 className='font-size-24 font-weight-bold border-bottom'>Experience</h2>
+                <span className='font-size-18 font-weight-bold'>StoryFit</span>
+                <div className='d-flex flex-column align-items-start'>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>Intern</span>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>Jan 2020 - May 2020</span>
+                    </p>
+                    <span>Utilized critical thinking to regulate movie scripts and format them correctly to ensure AI runs smoothly when determining optimal markets and audience</span>
+                </div>
+            </div>
 
-    {/* Education */ }
-    <div className='d-flex flex-column'>
-        <h2 className='font-size-24 font-weight-bold border-bottom'>Education</h2>
+            {/* Education */}
+            <div className='d-flex flex-column'>
+                <h2 className='font-size-24 font-weight-bold border-bottom'>Education</h2>
 
-        <span className='font-size-18 font-weight-bold'>BSA in Arts and Entertainment Technologies</span>
-        <div className='d-flex flex-column align-items-start'>
-            <p className='d-flex align-items-start'>
-                <span className='alert alert-primary mr-5 py-0 px-5'>University of Texas at Austin</span>
-                <span className='alert alert-primary mr-5 py-0 px-5'>Aug 2017 - May 2021</span>
-            </p>
-            <span>Graduated with a GPA of 3.51 with a concentration in Game Design and a Certificate in Radio Television and Film</span>
-        </div>
+                <span className='font-size-18 font-weight-bold'>BSA in Arts and Entertainment Technologies</span>
+                <div className='d-flex flex-column align-items-start'>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>University of Texas at Austin</span>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>Aug 2017 - May 2021</span>
+                    </p>
+                    <span>Graduated with a GPA of 3.51 with a concentration in Game Design and a Certificate in Radio Television and Film</span>
+                </div>
 
-        <span className='font-size-18 font-weight-bold mt-10'>Bootcamp Certificate</span>
-        <div className='d-flex flex-column align-items-start'>
-            <p className='d-flex align-items-start'>
-                <span className='alert alert-primary mr-5 py-0 px-5'>University of Texas at Austin</span>
-                <span className='alert alert-primary mr-5 py-0 px-5'>May 2021 - Dec 2021</span>
-            </p>
-            <span>A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js &amp; ReactJS</span>
-        </div>
-    </div>
+                <span className='font-size-18 font-weight-bold mt-10'>Bootcamp Certificate</span>
+                <div className='d-flex flex-column align-items-start'>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>University of Texas at Austin</span>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>May 2021 - Dec 2021</span>
+                    </p>
+                    <span>A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js &amp; ReactJS</span>
+                </div>
+            </div>
 
-    {/* Honors and Awards */ }
-    <div>
-        <h2 className='font-size-24 font-weight-bold border-bottom'>Honors and Awards</h2>
-        <span className='font-size-18 font-weight-bold mr-10 py-0 px-5'>Eagle Scout</span>
-        <div className='d-flex flex-column align-items-start'>
-            <p className='d-flex align-items-start'>
-                <span className='alert alert-primary mr-5 py-0 px-5'>Council Number - 576</span>
-                <span className='alert alert-primary mr-5 py-0 px-5'>Sept 2014</span>
-            </p>
-        </div>
-    </div>
+            {/* Honors and Awards */}
+            <div>
+                <h2 className='font-size-24 font-weight-bold border-bottom'>Honors and Awards</h2>
+                <span className='font-size-18 font-weight-bold mr-10 py-0 px-5'>Eagle Scout</span>
+                <div className='d-flex flex-column align-items-start'>
+                    <p className='d-flex align-items-start'>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>Council Number - 576</span>
+                        <span className='alert alert-primary mr-5 py-0 px-5'>Sept 2014</span>
+                    </p>
+                </div>
+            </div>
         </FadeIn >
     );
 }
