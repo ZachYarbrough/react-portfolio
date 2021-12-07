0599ca29899a8d2972df8780512933a18b1d33e1
Zach Yarbrough <zachyarbro@gmail.com>
2021-12-07 12:20:41 -0600

    fix (Index): Switched to BrowserRouter due to elongated URL

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';  
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <React.StrictMode>
+    <Router baseName={`/${process.env.PUBLIC_URL}`}>
+      <App />
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
